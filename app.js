8938b963e8415b9d271be0610578dffeeeb223a2
erictur <ericcode29@gmail.com>
2023-03-23 21:51:58 +0100

    Inicio del proyecto

---
+++ app.js
@@ -0,0 +1,8 @@
+const horaInin = document.querySelector('#horaInin','input[type="time"]');
+const horaInfin = document.querySelector('#horaInFin');
+
+leeDatos();
+function leeDatos(e){
+    const horaIN = horaInin.value;
+    console.log(horaIN);
+}
