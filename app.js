2560e64cf9921aa0251e51115cc532c0bcedcb7e
erictur <ericcode29@gmail.com>
2023-03-27 20:42:15 +0200

    Añadimos HTML y estilosCSS

--- app.js
+++
@@ -1,8 +0,0 @@
-const horaInin = document.querySelector('#horaInin','input[type="time"]');
-const horaInfin = document.querySelector('#horaInFin');
-
-leeDatos();
-function leeDatos(e){
-    const horaIN = horaInin.value;
-    console.log(horaIN);
-}
