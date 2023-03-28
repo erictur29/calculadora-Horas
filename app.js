c5bdbe29182ce4ab0b47105691e92e6e4e81c7c8
erictur <ericcode29@gmail.com>
2023-03-28 22:34:03 +0200

    empezamos un poco el JS

---
+++ app.js
@@ -0,0 +1,7 @@
+const horaIn = document.querySelector('#horaIn');
+const minIn = document.querySelector('#minIn');
+const horaFin = document.querySelector('#horaFin');
+const minFin = document.querySelector('#minFin');
+
+const tipo= document.querySelector('#tipo');
+const tipoJornada= document.querySelector('#tipoJornada');
