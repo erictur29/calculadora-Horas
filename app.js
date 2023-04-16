23f653c1012a2804e7551f8b600c47e9176db87a
erictur <ericcode29@gmail.com>
2023-04-16 20:22:35 +0200

    revisamos el correcto relleno de los inputs y simplificamos parte del codigo

--- app.js
+++ app.js
@@ -11,17 +11,14 @@ boton.addEventListener('click', horaTotal);
 
 
 function horaTotal(){
-    console.log('click')
+    console.log(horaFin.size)
    
     if (horaIn.value === '' || horaFin.value === ''  || minIn.value === ''  || minFin.value === ''){
-        const error = document.createElement('p');
-        error.textContent = 'Todos los campos son obligatorios';
-        error.classList.add('error');
-        document.querySelector('.principal').appendChild(error)
+        
+        mostrarMensaje('Todos los campos son obligatorios', 'error')
 
-        setTimeout(()=>{
-            error.remove();
-        }, 3000)
+        }else if (horaIn.value > 24 || horaFin.value > 24 || minIn.value > 60 || minFin.value > 60) {
+            mostrarMensaje('Los datos no son validos', 'error')
         } else {
             let minTotal = parseInt(minFin.value) - parseInt(minIn.value);
             let horaTotal = parseInt(horaFin.value) - parseInt(horaIn.value);
@@ -33,16 +30,33 @@ function horaTotal(){
             
             console.log(`las horas son ${horaTotal} y los minutos ${minTotal}`)
 
+            document.querySelector('#formulario').reset();
 
             // if (minTotal >= 60){
             //     horaTotal = horaTotal + 1;
             //     minTotal = minTotal - 60;
                
             // } este codigo valdrá en el futuro con la suma del registro
-        }
-
+        }   
         
     }
+
+    function datoCorrecto(){
+        
+    }
+
+    function mostrarMensaje(mensaje, clase){
+        const error = document.createElement('p');
+        error.textContent = mensaje;
+        error.classList.add(clase);
+        document.querySelector('.principal').appendChild(error)
+        setTimeout(()=>{
+            error.remove();
+        }, 3000)
+    }
+
+
+    
     
 
     
